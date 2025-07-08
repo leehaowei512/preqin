d8c3511026a95b7b3002065e266f4d105f966f3a
Hao Wei Lee <hao.lee@bp.com>
2025-07-08 23:58:28 +0100

    api endpoint: standardize httpexception

--- backend/api.py
+++ backend/api.py
@@ -38,7 +38,7 @@ def get_investor_summary(db: Session = Depends(get_db)):
 
     if results is None:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail=f"The investors data you requested for does not exist",
         )
     return results
